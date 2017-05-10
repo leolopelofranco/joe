be74b09855c46c7f62cbdae98710fbccefaae7bd
Leo <leolopelofranco@gmail.com>
2017-05-10 11:45:29 +0800

    a

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,6 +25,44 @@ class MessagesController < ApplicationController
     }
   end
 
+  def palm_sms
+    phone_number = 639175314928
+    email = params[:email]
+    mobile = params[:mobile]
+    note = params[:note]
+    name = params[:name]
+    message_type = 'SEND'
+    request_id = 0
+
+    message = "#{name} just inquired on Palm. Contact details are #{email} and #{mobile}. He said #{note}."
+
+    x = ChikkaModule.send_sms(phone_number, message, message_type, request_id)
+
+
+    render json: {
+      result: x,
+      status: 'success'
+    }
+  end
+
+  def palm_code
+    mobile = params[:mobile]
+    name = params[:name]
+    code = params[:code]
+    message_type = 'SEND'
+    request_id = 0
+
+    message = "Hi #{name}, your verification code is #{code}."
+
+    x = ChikkaModule.send_sms(mobile, message, message_type, request_id)
+
+
+    render json: {
+      result: x,
+      status: 'success'
+    }
+  end
+
   def get_s3_upload_key
 
     require 'base64'
@@ -72,46 +110,46 @@ class MessagesController < ApplicationController
 
     if params['message_type'] = 'INCOMING' || params['message_type'] = 'incoming'
 
-      if params['message'].strip.upcase == 'YES'
+      reply_message = params['message']
 
-        users = User.all.select{|x| x.mobile == params['mobile_number'][2..-1] }
+      users = User.all.select{|x| x.mobile == params['mobile_number'][2..-1] }
 
-        users.each do |user|
-          alarms = user.alarms.select{|x| x.status == 'sent' }
-          unless alarms.empty?
-            time = Time.now.to_i
-            alarm_taken = alarms.sort_by{ |x| "ABS(x.alarm.to_i - time)"}.first
-            alarm_taken.status = 'taken'
-            alarm_taken.save!
+      users.each do |user|
+        alarms = user.alarms.select{|x| x.status == 'sent' }
+        unless alarms.empty?
+          time = Time.now.to_i
+          alarm_taken = alarms.sort_by{ |x| "ABS(x.alarm.to_i - time)"}.first
+          alarm_taken.status = 'taken'
+          alarm_taken.save!
 
-            phone_number = params['mobile_number']
-            message = 'Thank you. Response noted.'
-            message_type = 'REPLY'
-            request_id = params['request_id']
+          phone_number = params['mobile_number']
+          message = 'Thank you. Response noted.'
+          message_type = 'REPLY'
+          request_id = params['request_id']
 
-            Rails.logger.info phone_number
-            Rails.logger.info message
-            Rails.logger.info message_type
-            Rails.logger.info request_id
+          Rails.logger.info phone_number
+          Rails.logger.info message
+          Rails.logger.info message_type
+          Rails.logger.info request_id
 
-            ChikkaModule.send_sms(phone_number, message, message_type, request_id)
+          ChikkaModule.send_sms(phone_number, message, message_type, request_id)
+          ChikkaModule.send_sms('+639175314928', reply_message, 'SEND', 0)
 
-
-          end
         end
-      else
-        phone_number = params['mobile_number']
-        message = 'Wrong Keyword. Reply YES if taken. If not, feel free to ignore this message.'
-        message_type = 'SEND'
-        request_id = 0
-
-        Rails.logger.info phone_number
-        Rails.logger.info message
-        Rails.logger.info message_type
-        Rails.logger.info request_id
-
-        ChikkaModule.send_sms(phone_number, message, message_type, request_id)
       end
+      # else
+      #   phone_number = params['mobile_number']
+      #   message = 'Wrong Keyword. Reply YES if taken. If not, feel free to ignore this message.'
+      #   message_type = 'SEND'
+      #   request_id = 0
+      #
+      #   Rails.logger.info phone_number
+      #   Rails.logger.info message
+      #   Rails.logger.info message_type
+      #   Rails.logger.info request_id
+      #
+      #   ChikkaModule.send_sms(phone_number, message, message_type, request_id)
+      # end
 
 
     end
